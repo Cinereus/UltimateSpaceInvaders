9f5292c46a890fa1cc5144486b65820908261311
DeadlySpinach <9featherbrain6@gmail.com>
2020-06-11 02:16:15 +0600

    Added project files

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public void FixedUpdate()
+    {
+        transform.Rotate(Vector3.up* Time.deltaTime*100, Space.Self);
+    }
+}
